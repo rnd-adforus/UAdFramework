954f02ed60c6d74b90cc07ed5fef34d9ebae7c66
rnd-adforus <rnd@adforus.co.kr>
2024-04-16 10:14:35 +0900

    1.0.0 Update

---
+++ Frameworks/UAdFramework.xcframework/ios-arm64/UAdFramework.framework/Headers/UAdFramework.h
@@ -0,0 +1,18 @@
+//
+//  UAdFramework.h
+//  UAdFramework
+//
+//  Created by adforus on 3/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UAdFramework.
+FOUNDATION_EXPORT double UAdFrameworkVersionNumber;
+
+//! Project version string for UAdFramework.
+FOUNDATION_EXPORT const unsigned char UAdFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UAdFramework/PublicHeader.h>
+
+
